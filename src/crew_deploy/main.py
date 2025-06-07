e350e52672304eabdca204b000ef6da09e4bfdb4
Antonio <antonio.elizabete59.gmail.com>
2025-06-06 23:46:16 -0300

    Envio do projeto_2

--- src/crew_deploy/main.py
+++ src/crew_deploy/main.py
@@ -6,27 +6,37 @@ from datetime import datetime
 
 from crew_deploy.crew import CrewDeploy
 
+# Essa linha diz ao Python para ignorar avisos do tipo SyntaxWarning 
+# (avisos de sintaxe) que venham do módulo chamado pysbd.
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
+# Este arquivo principal serve como uma maneira de você gerenciar sua
+# equipe localmente, portanto, evite adicionar lógica desnecessária a este arquivo.
+# Substitua pelas entradas que você deseja testar, pois ele irá interpolar automaticamente
+# quaisquer informações sobre tarefas e agentes
 
 def run():
     """
     Run the crew.
     """
+
+    # Cria um dicionário chamado inputs com dois dados, ou seja, com os dados das chaves
+    # que serão enviados como parametros para o programa ( apara os agentes e as tarefas)
+
+    # --  'topic': define o assunto como "AI LLMs" (Modelos de Linguagem de Inteligência Artificial).
+    # --  'current_year': pega o ano atual e transforma em string, usando datetime.now().year.
     inputs = {
         'topic': 'AI LLMs',
         'current_year': str(datetime.now().year)
     }
     
     try:
+        # Executa a crewAI passando dicionário  inputs como parametro
         CrewDeploy().crew().kickoff(inputs=inputs)
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
 
+# ------------------------------------------------------------------------------------
 
 def train():
     """
@@ -42,6 +52,8 @@ def train():
     except Exception as e:
         raise Exception(f"An error occurred while training the crew: {e}")
 
+# -------------------------------------------------------------------------------------
+
 def replay():
     """
     Replay the crew execution from a specific task.
@@ -51,6 +63,9 @@ def replay():
 
     except Exception as e:
         raise Exception(f"An error occurred while replaying the crew: {e}")
+
+
+# --------------------------------------------------------------------------------------
 
 def test():
     """
